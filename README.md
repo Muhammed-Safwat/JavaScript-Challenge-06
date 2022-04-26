76617a5b2374585f024bcf831fb922b9a8c3d7b3
Safwat <muhammedsafwat@gamil.com>
2022-04-26 16:34:19 +0200

    Background Image Slider Simple JavaScript Project

---
+++ README.md
@@ -0,0 +1,4 @@
+# JavaScript-Challenge-06
+<a href = "https://muhammed-safwat.github.io/JavaScript-Challenge-06/">Background Image Slider <a>Simple JavaScript Project<br>
+<b>Topics</b> :JavaScript(DOM Manipulation,Arrays, JavaScript CSS Manipulation, eventListeners) ,CSS and HTML . 
+ 
---
+++ main.js
@@ -0,0 +1,28 @@
+let imagesList = [ "1.webp", "2.webp",
+    "3.webp", "4.webp",
+    "5.webp", "6.webp",
+    "7.webp", "8.webp","9.webp" ,"10.webp","11.webp"];
+let img = document.querySelector( ".img" );
+let left = document.querySelector( ".left" );
+let right = document.querySelector( ".right" );
+
+let x = 0;
+right.onclick =  function (){
+    
+    if ( x === imagesList.length - 1 )
+    {
+        x = 0;
+    }
+    x++;
+    img.style.backgroundImage = `url("../images/${imagesList[x]}")` ;
+};
+left.onclick =  function (){
+    
+    if ( x === 0 )
+    {
+        x = imagesList.length-1;
+    }
+    x--;
+    console.log(imagesList[x]);
+    img.style.backgroundImage = `url("../images/${imagesList[x]}")` ;
+};
